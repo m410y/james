b82b60d6e2295784f9a9489d11726978372a7c9e
m410y <m410y@proton.me>
2024-03-02 17:48:58 +0700

    suppose fix and utf-8 abuse

--- eval.jl
+++ eval.jl
@@ -9,11 +9,11 @@ include("parse.jl")
 
 init = TOML.parsefile("init.toml")
 
-a, U = p4p_orient("$(init["p4p_folder"])/$(init["p4p_file"])")
-"a" in keys(init) && (a = init["a"])
+a, U = p4p_orient(joinpath(init["p4p_folder"], init["p4p_file"]))
+init["correct_lattice"] && (a = init["a"])
 @printf "Sample: %s\n" split(init["p4p_file"], ".")[1]
 
-meta = sfrm_meta("$(init["sfrm_folder"])/$(init["sfrm_files"][1])")
+meta = sfrm_meta(joinpath(init["sfrm_folder"], init["sfrm_files"][1]))
 
 drawing = init["drawing"]
 check = init["check"]
@@ -22,10 +22,10 @@ logging= init["logging"]
 
 wl = init["wl"]
 d = meta["d"]
-chi = meta["chi"]
-phi = meta["phi"]
-omega = meta["omega"]
-tth = meta["tth"]
+χ = meta["chi"]
+ϕ = meta["phi"]
+ω = meta["omega"]
+θ = meta["tth"]/2
 cols = meta["x_max"]
 rows = meta["y_max"]
 xc = init["xc"]
@@ -33,37 +33,37 @@ yc = init["yc"]
 px_size = init["px_size"]
 
 hank(ang) = rad2deg(rem2pi(ang, RoundNearest))
-@printf "   d    tth   omega    phi\n"
-@printf "%4.0lf %6.2lf  %6.2lf %6.2lf\n" d hank(tth) hank(omega) hank(phi)
+@printf "   d    2θ   ω    ϕ\n"
+@printf "%4.0lf %6.2lf  %6.2lf %6.2lf\n" d hank(2*θ) hank(ω) hank(ϕ)
 
 ray = [1, 0, 0]
-hkl = round.(Int, U'RotZXZ(phi, chi, -omega)*(RotZ(tth)*ray - ray)*a/wl[1])
+hkl = round.(Int, U'RotZXZ(ϕ, χ, -ω)*(RotZ(2*θ)*ray - ray)*a/wl[1])
 @printf "h k l: %d %d %d\n" hkl[1] hkl[2] hkl[3] 
 
-s = RotXZ(-chi, -phi)*U*hkl/a
+s = RotXZ(-χ, -ϕ)*U*hkl/a
 
-function true_omega(λ::Float64)::Float64
-    t_om = atan(s[2], s[1])*[1, 1] - acos(-0.5*λ*norm(s))*[1, -1]
-    diff = @. abs(rem2pi(t_om - omega, RoundNearest))
-    return diff[1] < diff[2] ? t_om[1] : t_om[2]
+function true_ω(λ::Float64)::Float64
+    _ω = atan(s[2], s[1])*[1, 1] - acos(-0.5*λ*norm(s))*[1, -1]
+    Δω = @. abs(rem2pi(_ω - ω, RoundNearest))
+    return Δω[1] < Δω[2] ? _ω[1] : _ω[2]
 end
 
 function coords(v::SVector{3, Float64})::SVector{2, Float64}
-    v = RotZ(-tth)*normalize(v)*d/px_size
+    v = RotZ(-2*θ)*normalize(v)*d/px_size
     return xc - v[2], yc + v[3]
 end
 
-peaks = "peaks" in keys(init) ? init["peaks"] : [coords(ray/λ + RotZ(true_omega(λ))*s) for λ in wl]
+peaks = "peaks" in keys(init) ? init["peaks"] : [coords(ray/λ + RotZ(true_ω(λ))*s) for λ in wl]
 
 x_diap = init["x_lims"][1]:init["x_lims"][2]
 y_diap = init["y_lims"][1]:init["y_lims"][2]
-image = sfrm_image("$(init["sfrm_folder"])/".*init["sfrm_files"])
+image = sfrm_image(joinpath.(init["sfrm_folder"], init["sfrm_files"]))
 fit_data = image[y_diap, x_diap]
 
 @printf "Pred. Ka1: %.2lf  %.2lf\n" peaks[1][1] peaks[1][2]
 @printf "Pred. Ka2: %.2lf  %.2lf\n" peaks[2][1] peaks[2][2]
 
-if drawing & check
+if drawing
     using Plots
 
     heatmap(x_diap, y_diap, fit_data, aspect_ratio=:equal)
@@ -90,6 +90,7 @@ xy_mesh = hcat(vec([x for y=y_diap, x=x_diap]), vec([y for y=y_diap, x=x_diap]))
 A0 = findmax(fit_data)[1]
 σ0 = init["sigma"]
 noise0 = init["noise"]
+
 g1 = [A0, peaks[1][1], peaks[1][2], σ0, σ0]
 g2 = [A0/2, peaks[2][1], peaks[2][2], σ0, σ0]
 p0 = [noise0; g1; g2]
@@ -104,14 +105,21 @@ if shift_corr
 
     @printf "Corr. Ka1: %.2lf  %.2lf\n" peaks[1][1] peaks[1][2]
     @printf "Corr. Ka2: %.2lf  %.2lf\n" peaks[2][1] peaks[2][2]
+
+    g1 = [A0, peaks[1][1], peaks[1][2], σ0, σ0]
+    g2 = [A0/2, peaks[2][1], peaks[2][2], σ0, σ0]
+    p0 = [noise0; g1; g2]
 end
 
 fit = curve_fit(fit_func, xy_mesh, vec(fit_data), p0)
 param = fit.param
-stdev = estimate_errors(fit)
-@printf "Params:      Int      x0      y0    σx    σy\n"
-@printf "Fit. Ka1: %6.0lf  %.2f  %.2f  %.2f  %.2f\n" param[2] param[3] param[4] param[5] param[6]
-@printf "Fit. Ka2: %6.0lf  %.2f  %.2f  %.2f  %.2f\n" param[7] param[8] param[9] param[10] param[11]
+stdev = stderror(fit)
+
+@printf "Params:      Int       x0       y0     σx     σy\n"
+@printf "Fit. Ka1: %6.0lf  %7.3f  %7.3f  %5.3f  %5.3f\n" param[2] param[3] param[4] param[5]  param[6]
+@printf "Fit. Ka2: %6.0lf  %7.3f  %7.3f  %5.3f  %5.3f\n" param[7] param[8] param[9] param[10] param[11]
+@printf "Err. Ka1: %6.0lf  %7.3f  %7.3f  %5.3f  %5.3f\n" stdev[2] stdev[3] stdev[4] stdev[5]  stdev[6]
+@printf "Err. Ka2: %6.0lf  %7.3f  %7.3f  %5.3f  %5.3f\n" stdev[7] stdev[8] stdev[9] stdev[10] stdev[11]
 
 if logging
     file = open("fit_log.txt", "a")
@@ -119,8 +127,8 @@ if logging
     write(file, "$(split(init["p4p_file"], ".")[1])\n")
     for idx in hkl; write(file, "$idx  "); end
     write(file, "\n")
-    tth, phi, omega = @. rad2deg(rem2pi([tth, phi, omega], RoundNearest))
-    write(file, "$d\t$tth\t$phi\t$omega\n")
+    θ, ϕ, ω = @. rad2deg(rem2pi([θ, ϕ, ω], RoundNearest))
+    write(file, "$d\t$(2*θ)\t$ϕ\t$ω\n")
     write(file, "$(fit.param[1])\n")
     for g_diap in [2:6, 7:11]
         for par in param[g_diap]; write(file, "$par\t"); end
--- suppose.jl
+++ suppose.jl
@@ -2,59 +2,83 @@ using StaticArrays
 using LinearAlgebra
 using Rotations
 using TOML
+using Printf
 using Combinatorics
-using IterTools
 
 include("parse.jl")
 
 
 init = TOML.parsefile("init.toml")
 
-a, U = p4p_orient("$(init["p4p_folder"])/$(init["p4p_file"])")
-"a" in keys(init) && (a = init["a"])
+a, U = p4p_orient(joinpath(init["p4p_folder"], init["p4p_file"]))
+init["correct_lattice"] && (a = init["a"])
+@printf "Sample: %s\n" split(init["p4p_file"], ".")[1]
 
-wl = init["wl"]
+λ = init["wl"][1]
+χ = deg2rad(init["chi"])
+
 hkl_max = init["hkl_max"]
-tth_min = deg2rad(init["tth_min"])
-tth_max = deg2rad(min(init["tth_max"], init["tth_lim"]))
-tth_om_delta = deg2rad(init["tth_om_delta"])
-chi = deg2rad(init["chi"])
-om_range = deg2rad(init["om_scan_range"])/2
+θ_min = deg2rad(init["tth_min"])/2
+θ_max = deg2rad(min(init["tth_max"], init["tth_lim"]))/2
+Δ_θω = deg2rad(init["tth_om_delta"])
+Δω = deg2rad(init["om_scan_range"])/2
 
-file = open("results.txt", "w")
-write(file, "h k l norm frid F tth phi om_0 om_m om_p om_mf om_pf\n")
-#for hkl in CartesianIndices((0:hkl_max, -hkl_max:hkl_max, -hkl_max:hkl_max))
-for hkl in [p.*s for p in permutations([20, 10, 4]), s in product([1, -1], [1, -1], [1, -1])]
-    hkl = SVector(Tuple(hkl))
-    hkl != [0,0,0] || continue
+hank(ang) = rad2deg(rem2pi(ang, RoundNearest))
+
+function valid_bond(ω_m, ω_p, θ)
+    valid(_ω, _θ) = abs(rem2pi(_ω - pi/2 - 2*_θ, RoundNearest)) > Δ_θω
+    return valid(ω_m-Δω, -θ) & valid(ω_m+Δω, -θ) & valid(ω_p-Δω, θ) & valid(ω_p+Δω, θ)
+end
+
+function process_hkl(hkl)
+    hkl != [0, 0, 0] || return
     
-    s = normalize(SVector{3, Float64}(U*hkl))
+    v = normalize(SVector(U*hkl))
+    abs(v[3]/sin(χ)) < 1 || return
+
     d = a/norm(hkl)
-    
-    (0.5*wl[1]/d < 1) ? (tth = 2*asin(0.5*wl[1]/d)) : continue
-    tth_min < tth < tth_max || continue
+    (0.5*λ/d < 1) ? (θ = asin(0.5*λ/d)) : return
+    θ_min < θ < θ_max || return
 
     ff = 1.0
-    ff > 1e-3 || continue
+    ff > 1e-3 || return
 
-    abs(s[3]/sin(chi)) < 1 || continue
-    for omega_0 in [-asin(s[3]/sin(chi)), pi+asin(s[3]/sin(chi))]
-        omega_0 -= om_range/2
-        v = RotXZ(chi, -omega_0)*[-1, 0, 0]
-        phi = atan(s[2], s[1])-atan(v[2], v[1])
-        omega_m = omega_0 + (pi - tth)/2
-        omega_p = omega_0 - (pi - tth)/2
-        omega_mf = omega_m + pi
-        omega_pf = omega_p + pi
-        valid(_om, _tth) = abs(rem2pi(_om - pi/2 - _tth, RoundNearest)) > tth_om_delta
-        valid_bond(_om_m, _om_p, _tth) = valid(_om_m, -_tth) & valid(_om_p, _tth)
-        val_norm = valid_bond(omega_m, omega_p, tth) & valid_bond(omega_m+om_range, omega_p+om_range, tth)
-        val_frid = valid_bond(omega_mf, omega_pf, tth) & valid_bond(omega_mf+om_range, omega_pf+om_range, tth)
+    result = []
+    for ω_0 in [asin(v[3]/sin(χ)), pi - asin(v[3]/sin(χ))]
+        ray = RotXZ(χ, -ω_0)*[-1, 0, 0]
+        ϕ = atan(v[2], v[1])-atan(ray[2], ray[1])
         
+        ω_m  = ω_0 + pi/2 - θ
+        ω_p  = ω_0 - pi/2 + θ
+        ω_mf = ω_0 - pi/2 - θ
+        ω_pf = ω_0 + pi/2 + θ
+        
+        v_n = valid_bond(ω_m,  ω_p,  θ)
+        v_f = valid_bond(ω_mf, ω_pf, θ)
+
+        append!(result, (v_n, v_f, ff, θ, ϕ, ω_0, ω_m, ω_p, ω_mf, ω_pf))
+    end
+
+    return result
+end
+
+function signperm(h, k, l)
+    pm = [1, -1]
+    return [perm.*signs for perm in permutations([h, k, l]), signs in [[i, j, k] for i=pm, j=pm, k=pm]]
+end
+
+file = open("suppose_$(split(init["p4p_file"], ".")[1]).txt", "w")
+write(file, "h\tk\tl\tv_n\tv_f\tff\t2θ\tϕ\tω_0\tω_m\tω_p\tω_mf\tω_p\n")
+
+for hkl in signperm(20, 10, 4)
+    hkl[1] < 0 && continue
+    result = process_hkl(hkl)
+    isnothing(result) && continue
+    for res in [result[1:10], result[11:20]]
+        v_n, v_f, ff, θ, ϕ, ω_0, ω_m, ω_p, ω_mf, ω_pf = res
         for idx in hkl; write(file, "$idx\t"); end
-        write(file, "$val_norm\t$val_frid\t$ff")
-        angles = rad2deg.(rem2pi.((tth, phi, omega_0, omega_m, omega_p, omega_mf, omega_pf), RoundNearest))
-        for ang in angles; write(file, "\t$ang"); end
+        write(file, "$v_n\t$v_f\t$ff")
+        for ang in hank.([2*θ, ϕ, ω_0, ω_m, ω_p, ω_mf, ω_pf]); write(file, "\t$ang"); end
         write(file, "\n")
     end
 end
